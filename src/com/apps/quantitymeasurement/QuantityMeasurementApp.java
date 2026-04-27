e0a4f17596ca72d1a1eb3d9243e77e1d0a671749
nitish-m12 <nitishm2020@gmail.com>
2026-04-27 11:31:13 +0530

    feature6: small change

--- src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -6,7 +6,7 @@ public class QuantityMeasurementApp {
     public static boolean demonstrateLengthEquality(Length l1, Length l2) {
         return l1.equals(l2);
     }
-
+    // UC6 addition implemented
     public static void demonstrateFeetEquality() {
         Length l1 = new Length(1.0, Length.LengthUnit.FEET);
         Length l2 = new Length(1.0, Length.LengthUnit.FEET);
